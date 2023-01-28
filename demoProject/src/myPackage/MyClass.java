dab653e3062a0c6f9f8c0091c96bdac125b5767a
Mininathlanghe <mininathlanghe1@gmail.com>
2023-01-28 19:26:48 +0530

    Basic Task

---
+++ demoProject/src/myPackage/MyClass.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class MyClass {
+    public static void main(String[]args)
+    {
+    	System.out.println("Testbase");
+    	System.out.println("Pom.xml");
+    	System.out.println("Utility");
+    	System.out.println("Resoures");
+    	System.out.println("suite");
+    }
+}
